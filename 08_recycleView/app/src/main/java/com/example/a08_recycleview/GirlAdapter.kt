9c2a684666fc17f77b7b502c879b69cf547f2d8e
pjhubs <877302410@qq.com>
2020-10-26 23:34:41 +0800

    08_recycleView 添加点击事件

--- 08_recycleView/app/src/main/java/com/example/a08_recycleview/GirlAdapter.kt
+++ 08_recycleView/app/src/main/java/com/example/a08_recycleview/GirlAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a07_listview.Girl
 
@@ -17,7 +18,19 @@ class GirlAdapter(val girls: List<Girl>): RecyclerView.Adapter<GirlAdapter.ViewH
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.girl_item, parent, false)
-        return ViewHolder(view)
+        val viewHolder = ViewHolder(view)
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            val girl = girls[position]
+            Toast.makeText(parent.context, "itemViewClick ${girl.name}", Toast.LENGTH_SHORT).show()
+        }
+        viewHolder.girlImage.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            val girl = girls[position]
+            Toast.makeText(parent.context, "girlAvatarClick ${girl.name}", Toast.LENGTH_SHORT).show()
+        }
+
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
